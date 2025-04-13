427243aa756a19cfb8eed98de037fd52e364f6a5
Éloi Strée <eloistree@gmail.com>
2025-04-13 21:55:53 +0200

    Save: 2025_04_13_21_55

--- Runtime/Primitive/RelayMono_Bool.cs
+++ Runtime/Primitive/RelayMono_Bool.cs
@@ -1,27 +1,9 @@
-﻿using UnityEngine.Events;
-
-namespace Eloi.Relay
+﻿namespace Eloi.Relay
 {
     public class RelayMono_Bool : AbstractGenericRelayMono<bool> {
 
 
-        public UnityEvent m_onRelayIsTrue;
-        public UnityEvent m_onRelayIsFalse;
-
-        protected override void HandleDataToRelayInChildren(bool dataToHandle)
-        {
-            if (dataToHandle)
-            {
-                m_onRelayIsTrue.Invoke();
-            }
-            else
-            {
-                m_onRelayIsFalse.Invoke();
-            }
-        }
-
     }
-
 
 
 
