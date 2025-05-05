05fa06fa16a36fb8b5df078ab2a76ff5127ac7dd
Éloi Strée <eloistree@gmail.com>
2025-05-05 20:52:21 +0200

    Save: 2025_05_05_20_52

---
+++ Runtime/Special/RelayMono_TextAssetAsStringAndLines.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Eloi.Relay
+{
+    public class RelayMono_TextAssetAsStringAndLines : AbstractGenericRelayMono<TextAsset>
+    {
+
+        public UnityEvent<string> m_onRelayedAsString = new UnityEvent<string>();
+        public UnityEvent<string[]> m_onRelayedAsLines = new UnityEvent<string[]>();
+
+        protected override void HandleDataToRelayInChildren(TextAsset dataToHandle)
+        {
+
+            // Example: Convert TextAsset to string and log it
+            string text = "";
+
+            if ( dataToHandle && dataToHandle.text!= null)
+            {
+                text = dataToHandle.text;
+            }
+            
+            string[] lines = text.Split(new[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+            m_onRelayedAsString.Invoke(text);
+        }
+    }
+
+
+
+
+}
+
